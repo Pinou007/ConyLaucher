0dc127a7ed80c12a32c7871c341bf12b2a8d7a0d
Pinou007 <lapipack@orange.fr>
2023-07-16 23:21:57 +0200

    Ajoutez des fichiers projet.

---
+++ LaucherGame/Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim CustomizableEdges5 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges6 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges7 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges8 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges1 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges2 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges3 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges4 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Guna2BorderlessForm1 = New Guna.UI2.WinForms.Guna2BorderlessForm(components)
+        Panel2 = New Panel()
+        Guna2Button4 = New Guna.UI2.WinForms.Guna2Button()
+        Guna2Button3 = New Guna.UI2.WinForms.Guna2Button()
+        PictureBox2 = New PictureBox()
+        Guna2Button2 = New Guna.UI2.WinForms.Guna2Button()
+        Guna2Button1 = New Guna.UI2.WinForms.Guna2Button()
+        Label1 = New Label()
+        Settings = New Panel()
+        Game = New Panel()
+        Panel1 = New Panel()
+        Panel2.SuspendLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        Game.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Guna2BorderlessForm1
+        ' 
+        Guna2BorderlessForm1.BorderRadius = 20
+        Guna2BorderlessForm1.ContainerControl = Me
+        Guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6R
+        Guna2BorderlessForm1.TransparentWhileDrag = True
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.FromArgb(CByte(38), CByte(38), CByte(38))
+        Panel2.Controls.Add(Guna2Button4)
+        Panel2.Controls.Add(Guna2Button3)
+        Panel2.Location = New Point(-2, 58)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(281, 568)
+        Panel2.TabIndex = 1
+        ' 
+        ' Guna2Button4
+        ' 
+        Guna2Button4.CustomizableEdges = CustomizableEdges5
+        Guna2Button4.DisabledState.BorderColor = Color.DarkGray
+        Guna2Button4.DisabledState.CustomBorderColor = Color.DarkGray
+        Guna2Button4.DisabledState.FillColor = Color.FromArgb(CByte(169), CByte(169), CByte(169))
+        Guna2Button4.DisabledState.ForeColor = Color.FromArgb(CByte(141), CByte(141), CByte(141))
+        Guna2Button4.FillColor = Color.Transparent
+        Guna2Button4.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Guna2Button4.ForeColor = Color.White
+        Guna2Button4.Image = CType(resources.GetObject("Guna2Button4.Image"), Image)
+        Guna2Button4.ImageAlign = HorizontalAlignment.Left
+        Guna2Button4.ImageSize = New Size(25, 25)
+        Guna2Button4.Location = New Point(0, 500)
+        Guna2Button4.Name = "Guna2Button4"
+        Guna2Button4.ShadowDecoration.CustomizableEdges = CustomizableEdges6
+        Guna2Button4.Size = New Size(281, 56)
+        Guna2Button4.TabIndex = 17
+        Guna2Button4.Text = "Settings"
+        Guna2Button4.TextAlign = HorizontalAlignment.Left
+        ' 
+        ' Guna2Button3
+        ' 
+        Guna2Button3.CustomizableEdges = CustomizableEdges7
+        Guna2Button3.DisabledState.BorderColor = Color.DarkGray
+        Guna2Button3.DisabledState.CustomBorderColor = Color.DarkGray
+        Guna2Button3.DisabledState.FillColor = Color.FromArgb(CByte(169), CByte(169), CByte(169))
+        Guna2Button3.DisabledState.ForeColor = Color.FromArgb(CByte(141), CByte(141), CByte(141))
+        Guna2Button3.Dock = DockStyle.Top
+        Guna2Button3.FillColor = Color.Transparent
+        Guna2Button3.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Guna2Button3.ForeColor = Color.White
+        Guna2Button3.Image = CType(resources.GetObject("Guna2Button3.Image"), Image)
+        Guna2Button3.ImageAlign = HorizontalAlignment.Left
+        Guna2Button3.ImageSize = New Size(25, 25)
+        Guna2Button3.Location = New Point(0, 0)
+        Guna2Button3.Name = "Guna2Button3"
+        Guna2Button3.ShadowDecoration.CustomizableEdges = CustomizableEdges8
+        Guna2Button3.Size = New Size(281, 56)
+        Guna2Button3.TabIndex = 0
+        Guna2Button3.Text = "Super Jeux"
+        Guna2Button3.TextAlign = HorizontalAlignment.Left
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.InitialImage = CType(resources.GetObject("PictureBox2.InitialImage"), Image)
+        PictureBox2.Location = New Point(12, 12)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(44, 36)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 0
+        PictureBox2.TabStop = False
+        ' 
+        ' Guna2Button2
+        ' 
+        Guna2Button2.BackColor = Color.Transparent
+        Guna2Button2.BorderRadius = 5
+        Guna2Button2.CustomizableEdges = CustomizableEdges1
+        Guna2Button2.DisabledState.BorderColor = Color.DarkGray
+        Guna2Button2.DisabledState.CustomBorderColor = Color.DarkGray
+        Guna2Button2.DisabledState.FillColor = Color.FromArgb(CByte(169), CByte(169), CByte(169))
+        Guna2Button2.DisabledState.ForeColor = Color.FromArgb(CByte(141), CByte(141), CByte(141))
+        Guna2Button2.FillColor = Color.Transparent
+        Guna2Button2.Font = New Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        Guna2Button2.ForeColor = Color.Transparent
+        Guna2Button2.Image = CType(resources.GetObject("Guna2Button2.Image"), Image)
+        Guna2Button2.ImageSize = New Size(25, 25)
+        Guna2Button2.Location = New Point(921, 12)
+        Guna2Button2.Name = "Guna2Button2"
+        Guna2Button2.PressedColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        Guna2Button2.ShadowDecoration.CustomizableEdges = CustomizableEdges2
+        Guna2Button2.Size = New Size(40, 30)
+        Guna2Button2.TabIndex = 14
+        ' 
+        ' Guna2Button1
+        ' 
+        Guna2Button1.BackColor = Color.Transparent
+        Guna2Button1.BorderColor = Color.Transparent
+        Guna2Button1.BorderRadius = 5
+        Guna2Button1.CustomizableEdges = CustomizableEdges3
+        Guna2Button1.DisabledState.BorderColor = Color.DarkGray
+        Guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray
+        Guna2Button1.DisabledState.FillColor = Color.FromArgb(CByte(169), CByte(169), CByte(169))
+        Guna2Button1.DisabledState.ForeColor = Color.FromArgb(CByte(141), CByte(141), CByte(141))
+        Guna2Button1.FillColor = Color.Transparent
+        Guna2Button1.Font = New Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        Guna2Button1.ForeColor = Color.Transparent
+        Guna2Button1.Image = CType(resources.GetObject("Guna2Button1.Image"), Image)
+        Guna2Button1.ImageSize = New Size(25, 25)
+        Guna2Button1.Location = New Point(967, 12)
+        Guna2Button1.Name = "Guna2Button1"
+        Guna2Button1.PressedColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        Guna2Button1.ShadowDecoration.CustomizableEdges = CustomizableEdges4
+        Guna2Button1.Size = New Size(50, 30)
+        Guna2Button1.TabIndex = 13
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(57, 16)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(55, 25)
+        Label1.TabIndex = 15
+        Label1.Text = "Cony"
+        ' 
+        ' Settings
+        ' 
+        Settings.Location = New Point(281, 58)
+        Settings.Name = "Settings"
+        Settings.Size = New Size(765, 575)
+        Settings.TabIndex = 16
+        Settings.Visible = False
+        ' 
+        ' Game
+        ' 
+        Game.BackgroundImage = CType(resources.GetObject("Game.BackgroundImage"), Image)
+        Game.BackgroundImageLayout = ImageLayout.Stretch
+        Game.Controls.Add(Panel1)
+        Game.Location = New Point(275, 58)
+        Game.Name = "Game"
+        Game.Size = New Size(765, 556)
+        Game.TabIndex = 17
+        Game.Visible = False
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Location = New Point(351, 418)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(361, 98)
+        Panel1.TabIndex = 0
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(31), CByte(31), CByte(31))
+        ClientSize = New Size(1029, 617)
+        Controls.Add(Game)
+        Controls.Add(Settings)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox2)
+        Controls.Add(Guna2Button2)
+        Controls.Add(Guna2Button1)
+        Controls.Add(Panel2)
+        FormBorderStyle = FormBorderStyle.None
+        Name = "Form1"
+        Text = "Form1"
+        Panel2.ResumeLayout(False)
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        Game.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Guna2BorderlessForm1 As Guna.UI2.WinForms.Guna2BorderlessForm
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Guna2Button2 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Guna2Button1 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Guna2Button3 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Guna2Button4 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Settings As Panel
+    Friend WithEvents Game As Panel
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ LaucherGame/Form1.vb
@@ -0,0 +1,104 @@
+﻿Imports Guna.UI2.WinForms.Suite
+Imports System.Windows.Forms
+Imports System.IO
+Imports System
+Imports System.Diagnostics
+Imports System.Net.NetworkInformation
+Imports Guna.UI2.WinForms
+Imports Microsoft.Win32
+Imports System.Runtime.InteropServices
+Imports System.Diagnostics.Metrics
+Imports System.Net
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Code exécuté lors du chargement du formulaire
+    End Sub
+
+    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
+        ' Bouton "Fermer"
+        Me.Hide() ' Masquer le formulaire actuel
+    End Sub
+
+    Private Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles Guna2Button2.Click
+        ' Bouton "Minimiser"
+        Me.WindowState = FormWindowState.Minimized ' Réduire la fenêtre à l'icône de la barre des tâches
+    End Sub
+
+    Private Sub Guna2Button3_Click(sender As Object, e As EventArgs) Handles Guna2Button3.Click
+        ' Bouton "Afficher Game"
+        Settings.Visible = False ' Masquer le panneau des paramètres
+        Game.Visible = True ' Afficher le panneau du jeu
+
+
+
+
+        ' Spécifie l'URL de la page à télécharger
+        Dim url As String = "https://raw.githubusercontent.com/Pinou007/Cony-Game-Public/main/app/version/ProjectSpike/version-game"
+
+        ' Spécifie le chemin complet du fichier de destination
+        Dim filePath As String = Path.Combine(Application.StartupPath, "laucher\project-spike\info\game_version.cony")
+
+        ' Crée un objet WebClient pour télécharger le contenu de la page
+        Dim client As New WebClient()
+
+        ' Télécharge le contenu de la page et le stocke dans une variable
+        Dim htmlContent As String = client.DownloadString(url)
+
+        ' Écrit le contenu téléchargé dans le fichier de destination
+        File.WriteAllText(filePath, htmlContent)
+
+        ' Affiche un message de confirmation
+        Console.WriteLine("Le fichier HTML a été téléchargé avec succès.")
+        Console.ReadLine()
+
+
+
+
+
+
+
+        ' Chemin du fichier contenant la version du jeu
+        Dim gameVersionFile As String = Path.Combine(Application.StartupPath, "laucher\project-spike\info\game_version.cony")
+
+        ' Chemin du fichier contenant la version installée du jeu
+        Dim installedVersionFile As String = Path.Combine(Application.StartupPath, "laucher\project-spike\info\game_version_install.cony")
+
+        ' Vérifier si les fichiers de version existent
+        If File.Exists(gameVersionFile) AndAlso File.Exists(installedVersionFile) Then
+            ' Lire le contenu du fichier de version du jeu
+            Dim gameVersion As String = File.ReadAllText(gameVersionFile)
+
+            ' Lire le contenu du fichier de version installée du jeu
+            Dim installedVersion As String = File.ReadAllText(installedVersionFile)
+
+            ' Comparer les numéros de version
+            If String.Compare(gameVersion, installedVersion) > 0 Then
+                ' Afficher un message si la version du jeu est supérieure à la version installée
+                MsgBox("La version du jeu est supérieure à la version installée.")
+            ElseIf String.Compare(gameVersion, installedVersion) < 0 Then
+                ' Afficher un message si la version du jeu est inférieure à la version installée
+                MsgBox("La version du jeu est inférieure à la version installée.")
+            Else
+                ' Afficher un message si la version du jeu est égale à la version installée
+                MsgBox("La version du jeu est égale à la version installée.")
+            End If
+        Else
+            ' Afficher un message si les fichiers de version sont introuvables
+            MsgBox("Les fichiers de version sont introuvables.")
+
+        End If
+
+
+
+
+
+    End Sub
+
+    Private Sub Guna2Button4_Click(sender As Object, e As EventArgs) Handles Guna2Button4.Click
+        ' Bouton "Afficher Paramètres"
+        Settings.Visible = True ' Afficher le panneau des paramètres
+        Game.Visible = False ' Masquer le panneau du jeu
+    End Sub
+End Class
